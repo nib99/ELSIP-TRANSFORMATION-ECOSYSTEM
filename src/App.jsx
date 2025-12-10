b02b1c78da22d1bf7102f045a0baac3ff6aa30d5
Nibras Ahmed <odaabultum116@gmail.com>
2025-12-10 05:03:09 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
 
-import DashboardView from './views/DashboardView.jsx';
-import CredentialView from './views/CredentialView.jsx';
-import InspectionView from './views/InspectionView.jsx';
-import ExtendedInspectionView from './views/ExtendedInspectionView.jsx';
-import VideoView from './views/VideoView.jsx';
+import DashboardView from "./views/DashboardView";
+import VideoView from "./views/VideoView";
+import CredentialView from "./views/CredentialView";
+import InspectionView from "./views/InspectionView";
+import ExtendedInspectionView from "./views/ExtendedInspectionView";
 
 export default function App() {
   return (
-    <div className="app">
-      <Router>
-        <Routes>
-          <Route path="/" element={<DashboardView />} />
-          <Route path="/credentials" element={<CredentialView />} />
-          <Route path="/inspection" element={<InspectionView />} />
-          <Route path="/extended-inspection" element={<ExtendedInspectionView />} />
-          <Route path="/video" element={<VideoView />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <div className="flex min-h-screen bg-gray-100">
+        <Sidebar />
+        <div className="flex-1 flex flex-col">
+          <Header />
+          <main className="flex-1 overflow-auto p-6">
+            <Routes>
+              <Route path="/" element={<DashboardView />} />
+              <Route path="/video" element={<VideoView />} />
+              <Route path="/credentials" element={<CredentialView />} />
+              <Route path="/inspection" element={<InspectionView />} />
+              <Route path="/extended-inspection" element={<ExtendedInspectionView />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </Router>
   );
 }
